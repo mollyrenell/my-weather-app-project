54bfdd1ae9ba39cdc67177ce77aabb3c9ac1fc67
mollyrenell <mollyrenell@gmail.com>
2023-11-09 12:45:54 -0700

    updated get forecast function and api integration

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -53,30 +55,50 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "ba2b4fec70ec3812340td59a4b4doa39";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecast = document.querySelector("#forecast");
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
 
   let forecastHtml = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="col weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">⛅</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src = "${
+            day.condition.icon_url
+          }" class="weather-forecast-icon" />
+          </div>
           <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature-max">
-              <strong>18°</strong>
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-            <div class="weather-forecast-temperature-min">12°</div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}°</div>
           </div>
         </div>
 
  `;
+    }
   });
 
   forecastHtml = forecastHtml + `</div>`;
@@ -84,4 +106,3 @@ function displayForecast() {
 }
 
 searchCity("Manitou Springs");
-displayForecast();
