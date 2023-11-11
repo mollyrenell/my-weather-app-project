6947d7ec13847a454e295a0ed0995e9df2971e06
mollyrenell <mollyrenell@gmail.com>
2023-11-11 11:12:15 -0700

    Updated CSS styling, adjusted formatting

--- src/index.js
+++ src/index.js
@@ -81,23 +81,21 @@ function displayForecast(response) {
       forecastHtml =
         forecastHtml +
         `
-  <div class="col weather-forecast-day">
+        <div class="col weather-forecast-day">
           <div class="weather-forecast-date">${formatDay(day.time)}</div>
           <img src = "${
             day.condition.icon_url
           }" class="weather-forecast-icon" />
-          </div>
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
               <strong>${Math.round(day.temperature.maximum)}°</strong>
+              <p>   </p>
             </div>
             <div class="weather-forecast-temperature">${Math.round(
               day.temperature.minimum
             )}°</div>
           </div>
-        </div>
-
- `;
+        </div>`;
     }
   });
 
